b030a1a73a1836d50f4d5b7d0ffdc5a41def6f64
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-03-19 14:46:20 -0300

    Config file

--- lists_organizer/__init__.py
+++ lists_organizer/__init__.py
@@ -1,5 +1,7 @@
 from models import ListOrganizer
 
+import json
+
 def txt_to_list(file_path) -> list:
     '''Create list using file .txt'''
     with open(file_path, 'r', encoding = 'utf-8') as file:
@@ -16,14 +18,22 @@ def save_list_as(listed_items, save_path = 'organized_list.txt'):
             file.write(str(item) + '\n')
         file.close()
 
-if __name__ == '__main__':
-    # CONFIGs:
-    file_path = 'lists/teste.txt'
-    save_path = 'lists/organized_list.txt'
+# LOAD CONFIGs:
+config_file_path = './lists_organizer/config.json'
+with open(config_file_path) as config_file:
+    configs = json.load(config_file)
+    config_file.close()
 
-    # CREATE LIST:
-    listed_items = txt_to_list(file_path)
-    # ORGANIZE LIST:
-    list_organizer = ListOrganizer(listed_items)
-    # SAVE LIST:
-    save_list_as(list_organizer.organized_list, save_path)
+# CREATE LIST:
+file_path = configs['file_path']
+listed_items = txt_to_list(file_path)
+
+# ORGANIZE LIST:
+is_sorted_list = configs['is_sorted_list']
+is_non_duplicate_list = configs['is_non_duplicate_list']
+list_organizer = ListOrganizer(listed_items = listed_items, is_sorted_list = is_sorted_list, is_non_duplicate_list = is_non_duplicate_list)
+organized_list = list_organizer.organized_list
+
+# SAVE LIST:
+save_path = configs['save_path']
+save_list_as(listed_items = organized_list, save_path = save_path)
